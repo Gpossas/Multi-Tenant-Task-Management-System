257da817dc88af990889db40b67bedd1afb7d19e
Gpossas <gpossas@protonmail.com>
2023-11-21 15:55:15 -0300

    update team model to be a linking table

--- accounts/models.py
+++ accounts/models.py
@@ -2,59 +2,49 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models, transaction
 
-class TeamManager( models.Manager ):
-    """Manager for the Team model. Also handles the account creation"""
+# TODO: Model to be added
+# class Role:
+#     roles = [
+#         ( 'Administrator', 'administrator' ),
+#         ( 'Frontend', 'frontend' ),
+#         ( 'Backend', 'backend' ),
+#         ( 'Designer', 'designer' ),
+#         ( 'Trainee', 'trainee' ),
+#     ]
 
-    @transaction.atomic
-    def create_account( self, team_name, username, password ):
-        """Creates a Team along with the User(with staff permission) and returns them both"""
-
-        team = Team( name=team_name )
-        team.save()
-
-        user = User.objects.create_user(
-            username=username,
-            password=password,
-            team=team,
-            is_staff=True,
-        )
-
-        return team, user
+#     @classmethod
+#     def add_role( cls, role ):
+#         return cls.roles.append( role )
 
 
-class Team( models.Model ):
+class Tenant( models.Model ):
     id = models.UUIDField( primary_key=True, default=uuid.uuid4, editable=False )
-    name = models.CharField( 'name', max_length=100, unique=True )
-    created = models.DateTimeField(auto_now_add=True)
-
-    objects = TeamManager()
+    name = models.CharField( max_length=25, unique=True )
 
     class Meta:
-        db_table = 'teams'
+        db_table = 'tenants'
 
     def __str__( self ):
         return self.name
-    
 
-class Role:
-    roles = [
-        ( 'Administrator', 'administrator' ),
-        ( 'Frontend', 'frontend' ),
-        ( 'Backend', 'backend' ),
-        ( 'Designer', 'designer' ),
-        ( 'Trainee', 'trainee' ),
-    ]
-
-    @classmethod
-    def add_role( cls, role ):
-        return cls.roles.append( role )
 
 class User( AbstractUser ):
     id = models.UUIDField( primary_key=True, default=uuid.uuid4, editable=False )
-    team = models.ForeignKey( Team, related_name='users', on_delete=models.CASCADE, editable=False )
 
     class Meta:
         db_table = 'users'
 
     def __str__( self ):
-        return f'{ self.username } | { self.team.name }'
+        return self.username
+
+
+class Team( models.Model ):
+    created = models.DateTimeField( auto_now_add=True )
+    tenant = models.ForeignKey( Tenant, related_name='team', on_delete=models.CASCADE )
+    user = models.ForeignKey( User, related_name='team', on_delete=models.CASCADE )
+
+    class Meta:
+        db_table = 'teams'
+
+    def __str__( self ):
+        return self.tenant.name
