8002404af47aaaeab81b7a5a138d55735180deb9
Gpossas <gpossas@protonmail.com>
2023-11-21 17:29:06 -0300

    fix slash warning

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateUser
+from .views import CreateUser, CreateTenant
 
 urlpatterns = [
     path( '', CreateUser.as_view(), name='create_user' ),
+    path( 'create_tenant', CreateTenant.as_view(), name='create_tenant' ),
 ]
