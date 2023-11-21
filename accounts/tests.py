0071891e0e4f5ff7c8342ad035b6ae4d7e114734
Gpossas <gpossas@protonmail.com>
2023-11-21 15:11:21 -0300

    stage test for create account

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from rest_framework import status
+from django.urls import reverse
+
+client = Client()
 
 # Create your tests here.
+class CreateAccountTest( TestCase ):
+    def setUp( self ):
+        self.json = {
+            "user": {
+                "username": "mirio",
+                "password": "1000000"
+            },
+            
+            "team": {
+                "name": "Big Three"
+            }
+        }
+
+    def test_create_valid_team_and_account( self ):
+        pass
