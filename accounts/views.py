c1a9282a0c5cb57e2d4590a66a7663a237167587
Gpossas <gpossas@protonmail.com>
2023-11-21 08:06:05 -0300

    add create account view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+from .serializers import  AccountSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+User = get_user_model()
+
+class CreateAccount( APIView ):
+    """ Create account and team"""
+
+    def post( self, request ):
+        account = AccountSerializer( data=request.data )
+        if account.is_valid():
+            account.save()
+            return Response( account.data, status=status.HTTP_201_CREATED )
+        return Response( account.errors, status=status.HTTP_400_BAD_REQUEST )
