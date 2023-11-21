bdd05018ccfbafe97ef1c4d58790dd57a9b98828
Gpossas <gpossas@protonmail.com>
2023-11-21 17:46:31 -0300

    fix slash error

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CreateUser, CreateTenant
 
 urlpatterns = [
-    path( '', CreateUser.as_view(), name='create_user' ),
+    path( 'create_user', CreateUser.as_view(), name='create_user' ),
     path( 'create_tenant', CreateTenant.as_view(), name='create_tenant' ),
 ]
