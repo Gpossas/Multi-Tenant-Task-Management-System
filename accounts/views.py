66e2230b7f1e851a17ada7d6e21e7c059f6af618
Gpossas <gpossas@protonmail.com>
2023-11-21 16:17:41 -0300

    add create user view

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,18 @@
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
-from .serializers import  AccountSerializer
+from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
 User = get_user_model()
 
-class CreateAccount( APIView ):
-    """ Create account and team"""
+
+class CreateUser( APIView ):
+    """Create user"""
 
     def post( self, request ):
-        account = AccountSerializer( data=request.data )
-        if account.is_valid():
-            account.save()
-            return Response( account.data, status=status.HTTP_201_CREATED )
-        return Response( account.errors, status=status.HTTP_400_BAD_REQUEST )
+        deserialized_user = UserSerializer( data=request.data )
+        if deserialized_user.is_valid():
+            deserialized_user.save()
+            return Response( deserialized_user.data, status=status.HTTP_201_CREATED )
+        return Response( deserialized_user.errors, status=status.HTTP_400_BAD_REQUEST )
