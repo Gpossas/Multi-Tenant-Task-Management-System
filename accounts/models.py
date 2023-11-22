67c42a0918542f6bffc6f0de6d43edf4631ec9bb
Gpossas <gpossas@protonmail.com>
2023-11-22 11:01:19 -0300

    add tenant manager

--- accounts/models.py
+++ accounts/models.py
@@ -26,10 +26,26 @@ class User( AbstractUser ):
         return self.username
     
 
+class TenantManager( models.Manager ):
+    """Creates a Tenant along with the Team"""
+
+    @transaction.atomic
+    def create_tenant( self, tenant_name: str, user: User ):
+        tenant = Tenant.objects.create( name=tenant_name, owner=user )
+        tenant.save()
+
+        team = Team.objects.create( tenant=tenant, user=user )
+        team.save()
+
+        return tenant
+
+
 class Tenant( models.Model ):
     id = models.UUIDField( primary_key=True, default=uuid.uuid4, editable=False )
     name = models.CharField( max_length=25, unique=True )
     owner = models.ForeignKey( User, related_name='tenants', on_delete=models.SET_NULL, null=True )
+
+    objects = TenantManager()
 
     class Meta:
         db_table = 'tenants'
