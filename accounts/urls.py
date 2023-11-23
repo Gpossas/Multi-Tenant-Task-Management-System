f1e0a32000d3cbf25ecd98d535d947f130074e57
Gpossas <gpossas@protonmail.com>
2023-11-22 22:07:26 -0300

    add user detail endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateUser, CreateTenant
+from .views import CreateUser, CreateTenant, UserDetail
 
 urlpatterns = [
     path( 'create_user', CreateUser.as_view(), name='create_user' ),
+    path( 'user_detail/<str:username>', UserDetail.as_view(), name='user_detail' ),
+
     path( 'create_tenant', CreateTenant.as_view(), name='create_tenant' ),
 ]
