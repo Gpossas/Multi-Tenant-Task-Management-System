dc040c3174be273a6de2f7e193081d8f110c6c7d
Gpossas <gpossas@protonmail.com>
2023-11-24 13:35:58 -0300

    add join a team

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
-from .permissions import IsInCommonTeam, IsUser
+from .permissions import IsInCommonTeam, IsUser, IsInTeam
+from .models import Tenant, Team
 
 User = get_user_model()
 
@@ -68,3 +69,18 @@ class CreateTenant( APIView ):
             deserialized_tenant.save( owner=request.user )
             return Response( deserialized_tenant.data, status=status.HTTP_201_CREATED )
         return Response( deserialized_tenant.errors, status=status.HTTP_400_BAD_REQUEST )
+    
+
+class TeamDetail( APIView ):
+    permission_classes = ( IsAuthenticated, IsInTeam )
+    
+    #TODO: return tenant and new_member data in response
+    def post( self, request, team_name ):
+        """Join a user to a team. Must be someone in the team to add users"""
+
+        tenant = get_object_or_404( Tenant, name=team_name )
+        self.check_object_permissions( request, tenant )
+        new_member = get_object_or_404( User, username=request.data['username'] )
+
+        Team.objects.create( tenant=tenant, user=new_member )
+        return Response( status=status.HTTP_201_CREATED )
