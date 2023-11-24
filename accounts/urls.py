1ee129d44d944a1d4e6a81a9ddb656712921e116
Gpossas <gpossas@protonmail.com>
2023-11-24 13:35:44 -0300

    add team detail endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CreateUser, CreateTenant, UserDetail
+from .views import CreateUser, CreateTenant, UserDetail, TeamDetail
 
 urlpatterns = [
     path( 'create_user', CreateUser.as_view(), name='create_user' ),
     path( 'user_detail/<str:username>', UserDetail.as_view(), name='user_detail' ),
 
     path( 'create_tenant', CreateTenant.as_view(), name='create_tenant' ),
+    
+    path( 'team_detail/<str:team_name>', TeamDetail.as_view(), name='tenant_detail' ),
 ]
