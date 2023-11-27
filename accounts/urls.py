702c8b2bcadc90e26acfd2a9300df2f565266385
Gpossas <gpossas@protonmail.com>
2023-11-27 16:04:56 -0300

    update urls to model refactor

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import CreateUser, CreateTenant, UserDetail, TeamDetail
+from .views import CreateUser, TeamList, UserDetail, TeamDetail
 
 urlpatterns = [
     path( 'create_user', CreateUser.as_view(), name='create_user' ),
     path( 'user_detail/<str:username>', UserDetail.as_view(), name='user_detail' ),
 
-    path( 'create_tenant', CreateTenant.as_view(), name='create_tenant' ),
+    path( 'workspace', TeamList.as_view(), name='workspace' ),
     
-    path( 'team_detail/<str:team_name>', TeamDetail.as_view(), name='tenant_detail' ),
+    path( 'team_detail/<str:team_name>', TeamDetail.as_view(), name='team_detail' ),
 ]
