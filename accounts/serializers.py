5f4e29cb6b1c1f9c531a5ddebe471eaa5aeb5d37
Gpossas <gpossas@protonmail.com>
2023-11-27 19:23:46 -0300

    add representation to users

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,12 +7,16 @@ User = get_user_model()
 class UserSerializer( serializers.ModelSerializer ):
     class Meta:
         model = User
-        fields = ( 'id', 'username', 'password' )
+        fields = ( 'id', 'username', 'password', 'first_name', 'last_name', 'email' )
     
         # Make sure that the password field is never sent back to the client.
         extra_kwargs = {
             'password': { 'write_only': True },
         }
+    
+    def to_representation( self, instance ):
+        representation = super().to_representation( instance )
+        return { key: value for key, value in representation.items() if value }
     
     def create( self, validated_data ):
         return User.objects.create_user( **validated_data )
