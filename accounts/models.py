98a7c880c4c02e1399e3ba1f4ce81d54d00888b8
Gpossas <gpossas@protonmail.com>
2023-11-28 10:01:40 -0300

    add manager to support transactions

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db import transaction
 
 class User( AbstractUser ):
     id = models.UUIDField( primary_key=True, default=uuid.uuid4, editable=False )
@@ -12,11 +13,23 @@ class User( AbstractUser ):
         return self.username
     
 
+class TeamManager( models.Manager ):
+
+    @transaction.atomic
+    def create_team( self, team_name, captain ):
+        team = Team.objects.create( name=team_name )
+        team.save()
+        team.members.add( captain )
+        return team
+
+
 class Team( models.Model ):
     id = models.UUIDField( primary_key=True, default=uuid.uuid4, editable=False )
     name = models.CharField( max_length=30 )
     members = models.ManyToManyField( User, related_name='teams' )
     created = models.DateTimeField( auto_now_add=True )
+
+    objects = TeamManager()
 
     class Meta:
         db_table = 'teams'
