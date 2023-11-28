0a9f7b7a04127d2c004a02f0df645bc84065d8cd
Gpossas <gpossas@protonmail.com>
2023-11-28 16:33:16 -0300

    add jwt token endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import CreateUser, TeamList, UserDetail, TeamDetail
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+
     path( 'create_user', CreateUser.as_view(), name='create_user' ),
     path( 'user_detail/<str:username>', UserDetail.as_view(), name='user_detail' ),
 
