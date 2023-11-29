07f90fbbb6a8fec89fdbfcc19c637c5976d52395
Gpossas <gpossas@protonmail.com>
2023-11-29 19:07:31 -0300

    update class position

--- accounts/models.py
+++ accounts/models.py
@@ -3,16 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db import transaction
 
-class User( AbstractUser ):
-    id = models.UUIDField( primary_key=True, default=uuid.uuid4, editable=False )
-
-    class Meta:
-        db_table = 'users'
-
-    def __str__( self ):
-        return self.username
-    
-
 class TeamManager( models.Manager ):
 
     @transaction.atomic
@@ -22,6 +12,16 @@ class TeamManager( models.Manager ):
         team.members.add( captain )
         return team
 
+
+class User( AbstractUser ):
+    id = models.UUIDField( primary_key=True, default=uuid.uuid4, editable=False )
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__( self ):
+        return self.username
+    
 
 class Team( models.Model ):
     id = models.UUIDField( primary_key=True, default=uuid.uuid4, editable=False )
