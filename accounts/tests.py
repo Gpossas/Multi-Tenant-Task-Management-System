5bf0de1e1d3a3e76f325c4a2b6220c2faf41a7d6
gpossas <possasg@gmail.com>
2023-12-05 13:53:40 -0300

    add initial tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,22 +1,58 @@
-from django.test import TestCase, Client
 from rest_framework import status
 from django.urls import reverse
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 
-client = Client()
+User = get_user_model()
 
-# Create your tests here.
-class CreateAccountTest( TestCase ):
+class UserDetailTestCase( TestCase ):
     def setUp( self ):
-        self.json = {
-            "user": {
-                "username": "mirio",
-                "password": "1000000"
-            },
-            
-            "team": {
-                "name": "Big Three"
-            }
-        }
+        self.client = Client()
+        self.user = User.objects.create( username='luffy', first_name='Monkey', last_name='D. Luffy', password='123' )
 
-    def test_create_valid_team_and_account( self ):
+    def test_unauthenticated_user_forbidden( self ):
+        """Ensure all methods deny unauthenticated user access"""
+
+        url = reverse( 'user_detail', args=['luffy'] )
+
+        get_response = self.client.get( url )
+        post_response = self.client.post( url )
+        put_response = self.client.put( url )
+        delete_response = self.client.delete( url )
+        patch_response = self.client.patch( url )
+
+        self.assertEqual( status.HTTP_403_FORBIDDEN, get_response.status_code )
+        self.assertEqual( status.HTTP_403_FORBIDDEN, post_response.status_code )
+        self.assertEqual( status.HTTP_403_FORBIDDEN, put_response.status_code )
+        self.assertEqual( status.HTTP_403_FORBIDDEN, delete_response.status_code )
+        self.assertEqual( status.HTTP_403_FORBIDDEN, patch_response.status_code )
+
+    def test_not_owner_or_not_a_team_member_access_forbidden( self ):
         pass
+    def test_nonexistent_user( self ):
+        pass
+
+    # GET METHOD 
+    def test_valid_get_user_detail( self ):
+        """Test valid access to owner account detail"""
+
+        owner = Client()
+        owner.force_login( self.user )
+        url = reverse( 'user_detail', args=[self.user.username] )
+        get_response = owner.get( url )
+        self.assertEqual( status.HTTP_200_OK, get_response.status_code )
+
+    def test_get_user_unsigned( self ):
+        pass # forbidden
+    def test_get_user_signed_as_another_user( self ):
+        pass # forbidden
+    def test_get_nonexistent_user( self ):
+        pass # 404
+
+    # PUT METHOD
+    def test_valid_data_edit( self ):
+        pass
+    def test_edit_another_user_data( self ):
+        pass # forbidden
+    def test_update_password_with_wrong_method( self ):
+        pass # forbidden
