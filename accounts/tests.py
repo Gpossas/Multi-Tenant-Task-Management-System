3d8dbcfc37c4988c819c476bc6c29855eb9e9af5
gpossas <possasg@gmail.com>
2023-12-06 15:01:33 -0300

    add unexistent field test

--- accounts/tests.py
+++ accounts/tests.py
@@ -125,3 +125,10 @@ class UserDetailTestCase( TestCase ):
 
         response = self.client.put( url, data={ 'password': 'not_allowed' }, content_type='application/json' )
         self.assertEqual( status.HTTP_405_METHOD_NOT_ALLOWED, response.status_code )
+    
+    def test_put_unexistent_field( self ):
+        url = reverse( 'user_detail', args=['luffy'] )
+        self.client.force_login( self.luffy )
+
+        response = self.client.put( url, data={ 'none_field': 'not_allowed' }, content_type='application/json' )
+        self.assertEqual( status.HTTP_400_BAD_REQUEST, response.status_code )
