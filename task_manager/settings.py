d9f5bdf80c4c439ccfae69cb38556f72ce02e7e3
Fernando Leite <fernandogfleite@gmail.com>
2024-01-12 15:47:38 -0300

    Add configuration for CORS and CSRF protection

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,33 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s&c9kwj$e2g^d758g+nxbnzd0wn^6!#3$on!rv571$&t3w-w2('
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(',')])
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+CORS_ALLOWED_ORIGINS = config(
+    'ALLOWED_HOSTS_CORS', cast=lambda v: [s.strip() for s in v.split(', ')]
+)
+
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS', 
+    cast=lambda v: [s.strip() for s in v.split(', ')]
+)
+
+CORS_ALLOW_HEADERS = (
+    'content-disposition',
+    'accept-encoding',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization'
+)
+        
 
 
 # Application definition
@@ -40,6 +62,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'accounts',
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
@@ -53,6 +76,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
